16f0618d90bba801f82a2d3a186359331f857b29
gibidon <malykhinig@gmail.com>
2023-08-06 07:53:51 +0700

    compiled with JSX

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'; //  императивный
+import './App.css'; // императивный
+import { createElement } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const date = new Date().getFullYear().toString(); // императивный, шаги
+
+const App = () => {
+	//компоненты реакта по сути декларативные. Как и сам html
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+				<div>year now: {date}</div>
+			</header>
+		</div>
+	);
+};
 
 export default App;
