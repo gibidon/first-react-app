ca4b59a616b9fae6196279ba0810a44e134c9772
gibidon <malykhinig@gmail.com>
2023-08-09 20:51:01 +0700

    fixed indentation + comments

--- src/App.js
+++ src/App.js
@@ -1,38 +1,39 @@
-import logo from './logo.svg'; //  императивный
-import './App.css'; // императивный
-import { createElement } from 'react';
+import logo from "./logo.svg"; //  понял благодаря видео, это декларативный. Описывыем что импортировать
+import "./App.css"; //
+import { createElement } from "react";
 
 const date = new Date().getFullYear().toString(); // императивный, шаги
 
+// императивная часть, экспорт
 const App = () => {
-	// такое описание компонента куда более императивное конечно чем jsx
-	return createElement(
-		'div',
-		'App',
-		createElement(
-			'header',
-			{ className: 'App-header' },
-			createElement('img', { src: logo, className: 'App-logo', alt: 'logo' }),
-			createElement(
-				'p',
-				{},
-				'Edit ',
-				createElement('code', {}, 'src/App.js and save to reload'),
-				createElement(
-					'a',
-					{
-						className: 'App-link',
-						href: 'https://reactjs.org',
-						target: '_blank',
-						rel: 'noopener noreferrer',
-					},
-					createElement('br'),
-					'Learn React',
-				),
-				createElement('div', {}, `year now:${date}`),
-			),
-		),
-	);
+  return createElement(
+    // ретурн это еще логика, императивная
+    "div",
+    "App", // тут начинается деларативный. Ответ на вопрос ,,Что мы хотим получить"
+    createElement(
+      "header",
+      { className: "App-header" },
+      createElement("img", { src: logo, className: "App-logo", alt: "logo" }),
+      createElement(
+        "p",
+        {},
+        "Edit ",
+        createElement("code", {}, "src/App.js and save to reload"),
+        createElement(
+          "a",
+          {
+            className: "App-link",
+            href: "https://reactjs.org",
+            target: "_blank",
+            rel: "noopener noreferrer",
+          },
+          createElement("br"),
+          "Learn React",
+        ),
+        createElement("div", {}, `year now:${date}`),
+      ),
+    ),
+  );
 };
 
-export default App;
+export default App; //декларативный, команда экспорта - что хотим экспортировать
